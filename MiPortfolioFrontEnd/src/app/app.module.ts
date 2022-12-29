42cbebdee53439bf1ffbea1c215fb6eab3618c44
Kevin <kevrol457@gmail.com>
2022-12-29 13:22:34 -0300

    agregado componente encabezado

--- MiPortfolioFrontEnd/src/app/app.module.ts
+++ MiPortfolioFrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Micomponente } from './components/mi-componente/mi-componente.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Micomponente
+    Micomponente,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule,
